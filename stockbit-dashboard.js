a718a181e46b69776c2a13095cd973cd1f242dd5
gdp311201 <growspheredigpro@gmail.com>
2026-08-28 17:34:09 +0700

    Update stockbit-dashboard.js

--- stockbit-dashboard.js
+++ stockbit-dashboard.js
@@ -21,7 +21,7 @@
             id: "14ryEGNhvwm9XCuw-lo6tSfwDqRmGAK_ZlUpuKd0Pm8M",
             sheet: "SC",
             startCol: "T",
-            gasUrl: "https://script.google.com/macros/s/AKfycby867iVRm0RlVnipq4obh9vaxfzy6nyIJ9DkENATabCCb4Af8G4ylQvxcWPgJWpg3OnRw/exec"
+            gasUrl: "https://script.google.com/macros/s/AKfycbxBmt9PR_jW3CwiiOKrel_clbUCTWGC2Br3ocvANT1pnrvqoqUr4HSuHNRhEYZZ0k7GHA/exec"
         },
         "BD SANGKUT": {
             targetKeywords: ["BD - SANGKUT & AKUM", "BD SANGKUT", "BD - SANGKUT"],
